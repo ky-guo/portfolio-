b3b611ca24bf3a0cf41082eccfd175626b3a94b8
ky-guo <kaiyingguo1210@Gmail.com>
2017-08-01 16:01:50 -0400

    day 4
    made a button using jS

---
+++ Kaiying's Portfolio.html/scripts.js
@@ -0,0 +1,6 @@
+// window.alert("Hi. ALL OF THIS IS RATHER INACCURATE. yeah. Click the ok button. u_u")
+//
+
+function moreFunction() {
+    document.getElementById("more").style.display= "list-item";
+};
